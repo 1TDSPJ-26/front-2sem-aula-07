dbb8c7d0a27a36807e868c5ad2b4de02ddaff6f6
Ch-Rodrigues <christianprodrigues07@gmail.com>
2026-09-03 11:52:42 -0300

    feat : Trabalhando com imagens, externas, estáticas e internas.

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,10 +1,44 @@
+import imgQuadrado from "../../img/quadrado.png";
+
 export function Conteudo() {
-    return (
-        <main>
-            <section>
-            <h2>Bem-vindo ao meu site!</h2>
-            <p>Este é o conteúdo principal da página.</p>
-            </section>
-        </main>
-    );
+  return (
+    <main>
+      <section>
+        <h2>Conteúdo</h2>
+        <figure>
+          <img
+            src="https://placehold.co/600x400/FFFFF0/FFFFFF/png"
+            alt="Imagem ilustrativa do conteúdo" />
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis accusantium id ipsa recusandae? Fugit vel perspiciatis neque sint magnam eveniet, voluptatum nemo non atque doloremque voluptates magni labore qui reprehenderit expedita, velit nisi tempore asperiores quia, natus amet a adipisci consequatur? Distinctio in maiores nobis repellendus, dolorem magni tenetur. Architecto?</p>
+      </section>
+      <section>
+        <h2>Imagem interna</h2>
+        <figure>
+          <img
+            src={imgQuadrado}
+            alt="Imagem ilustrativa do conteúdo" />
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus perferendis enim reiciendis numquam aliquam labore veritatis amet architecto incidunt porro quaerat beatae quas harum doloremque, magni dolore! Possimus, quidem, ratione odio ipsam eveniet fugiat molestiae quos doloribus dolores voluptatum quo, tenetur excepturi? Eaque, earum atque illo impedit fugiat tenetur sequi.</p>
+      </section>
+       <section>
+        <h2>Imagem Estática</h2>
+        <figure>
+          <img
+            src="/quadrado-b.png"
+            alt="Imagem ilustrativa do conteúdo" />
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus officia pariatur repellendus at quo velit, iure laborum, provident quaerat doloremque, exercitationem molestiae aspernatur ea quibusdam ipsum unde quidem mollitia nulla! Et odit eius dolorum pariatur atque hic ratione, id quae accusantium, debitis tempore quas ipsum itaque aut, veniam vel. Doloremque.</p>
+      </section>
+    </main>
+  );
 }
