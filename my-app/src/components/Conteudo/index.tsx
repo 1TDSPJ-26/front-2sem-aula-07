78057ba8d2d4094c5196dbc189a5afe061dcbd22
TailyniDev <tailyni80@gmail.com>
2026-09-03 12:08:37 -0300

    feat: adiciona componentes e favicon

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,28 +1,32 @@
-import imgQuadrado from '../../img/quadrado.png'
+import imgQuadrado from "../../img/quadrado.png";
 
 export default function Conteudo() {
   return (
     <main>
-      <section>
-        <h2>COnteudo</h2>
-        <figure>
-          <img src="https://placehold.co/600x400/FF0000/FFFFFF/png" alt="Imagem ilustrativa do conteudo" />
-          <figcaption>Imagem utilizada para repreesentar o conteúdo da aplicação</figcaption>
-        </figure>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, consequatur nam beatae aperiam odit maxime doloremque quod repellat veniam consequuntur hic laudantium fugiat, iure reprehenderit perspiciatis distinctio, laborum ab repellendus.
-        Ex ipsum voluptatibus minus inventore natus, assumenda quibusdam iste repudiandae dolores, dolor officia similique. Nostrum culpa quis harum. Culpa sed error ipsam libero optio porro corrupti ducimus quos iure repudiandae.
-        Voluptatem dicta rem sit inventore accusantium blanditiis quia, placeat alias dolorem saepe obcaecati veniam. Tempore a inventore quis itaque veritatis, nesciunt quasi amet repellendus voluptates minima id mollitia, excepturi earum.
-      </section>
-      <section>
-        <h2>Imagem Interna</h2>
-        <figure>
-          <img src={imgQuadrado} alt="Imagem ilustrativa do conteudo" />
-          <figcaption>Imagem utilizada para repreesentar o conteúdo da aplicação</figcaption>
-        </figure>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, consequatur nam beatae aperiam odit maxime doloremque quod repellat veniam consequuntur hic laudantium fugiat, iure reprehenderit perspiciatis distinctio, laborum ab repellendus.
-        Ex ipsum voluptatibus minus inventore natus, assumenda quibusdam iste repudiandae dolores, dolor officia similique. Nostrum culpa quis harum. Culpa sed error ipsam libero optio porro corrupti ducimus quos iure repudiandae.
-        Voluptatem dicta rem sit inventore accusantium blanditiis quia, placeat alias dolorem saepe obcaecati veniam. Tempore a inventore quis itaque veritatis, nesciunt quasi amet repellendus voluptates minima id mollitia, excepturi earum.
-      </section>
+        <section>
+            <h2>Conteúdo</h2>
+            <figure>
+              <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="Imagem ilustrativa do conteúdo" />
+              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
+            </figure>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
+        </section>
+         <section>
+            <h2>Imagem Interna</h2>
+            <figure>
+              <img src={imgQuadrado} alt="Imagem ilustrativa do conteúdo" />
+              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
+            </figure>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
+        </section>
+         <section>
+            <h2>Imagem Estática</h2>
+            <figure>
+              <img src="/quadrado-b.png" alt="Imagem ilustrativa do conteúdo" />
+              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
+            </figure>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
+        </section>
     </main>
-  );
+  )
 }
