2aaf082b8f9b92e42ef65c20528cd0156b172b52
chrisverbarbuto <chrisvbarbuto@gmail.com>
2026-09-03 11:36:55 -0300

    feat: Adicionando index.tsx ao cabecalho, componentes e rodape. Alteraçoes no App.tsx para importar os novos conteudos.

---
+++ my-app/src/components/Cabecalho/index.tsx
@@ -0,0 +1,14 @@
+export default function Cabecalho() {
+  return (
+    <header>
+        <h1>Meu site</h1>
+        <nav>
+          <ul>
+            <li>Texto-1</li>
+            <li>Texto-2</li>
+            <li>Texto-3</li>
+          </ul>
+        </nav>
+      </header>
+  )
+}   
---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,18 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <h2>Conteúdo principal</h2>
+
+      <figure>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_2IjWJNu2-_YphbSN1Ej2wsmsOA1YUk6cGV0RELR9oFt5715Ijy0uvZI&s=10"
+          alt="Imagem ilustrativa do conteúdo"
+        />
+
+        <figcaption>
+          Imagem utilizada para visualizar se esta funcionando corretamente.
+        </figcaption>
+      </figure>
+    </main>
+  )
+}
