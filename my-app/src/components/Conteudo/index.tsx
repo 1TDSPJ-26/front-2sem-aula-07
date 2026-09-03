e3a8431a8538d03eb157369910811b3e27d1ed01
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-03 11:51:28 -0300

    feat : Trabalhando com imagens, externas, internas e estáticas.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,10 +1,30 @@
-
+import imgQuadrado from "../../img/quadrado.png";
 
 export default function Conteudo() {
   return (
     <main>
         <section>
             <h2>Conteúdo</h2>
+            <figure>
+              <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="Imagem ilustrativa do conteúdo" />
+              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
+            </figure>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
+        </section>
+         <section>
+            <h2>Imagem Interna</h2>
+            <figure>
+              <img src={imgQuadrado} alt="Imagem ilustrativa do conteúdo" />
+              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
+            </figure>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
+        </section>
+         <section>
+            <h2>Imagem Estática</h2>
+            <figure>
+              <img src="/quadrado-b.png" alt="Imagem ilustrativa do conteúdo" />
+              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
+            </figure>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
         </section>
     </main>
