919b657ceb79f0373e035de2fd9787f213ef40d8
lucasvieira-tech <martivieiralucas@gmail.com>
2026-09-03 11:00:03 -0300

    feat:adiciona componetes e favicon

---
+++ my-app/src/components/Cabecalho/index.tsx
@@ -0,0 +1,9 @@
+function Cabecalho() {
+  return (
+    <header>
+      <h1>Meu Cabeçalho</h1>
+    </header>
+  );
+}
+
+export default Cabecalho;
---
+++ my-app/src/components/Rodape/index.tsx
@@ -0,0 +1,9 @@
+function Rodape() {
+  return (
+    <footer>
+      <p>Meu Rodapé</p>
+    </footer>
+  );
+}
+
+export default Rodape;
