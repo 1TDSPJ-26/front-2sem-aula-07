80eea784ed0e0e974a1c93bad7e97fd1716361a4
SophiaS4nts <sophia.sophis@icloud.com>
2026-09-03 11:52:30 -0300

    feat: trabalhando com imagens internas e estaticas, com criacão de cabeçlho, rodape e conteudo.

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,38 @@
+import imgQuadroado from "../../img/quadrado.png";
+
+export default function Conteudo() {
+  return (
+    <main>
+        <section>
+            <h2>Conteúdo Principal</h2>
+            <figure>
+                <img
+          src="https://placehold.co/600x400/FFFFF0/FFFFFF/png"
+          alt="Imagem ilustrativa do conteúdo"
+            />
+            </figure>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus 
+               sapiente tempora ex dicta ullam, sunt ea sit explicabo molestias similique 
+               corrupti unde ipsam magnam, illo atque voluptates deleniti placeat quod
+               dolor veniam. Dolorum modi, aperiam numquam architecto, unde reprehenderit 
+               similique iusto iure molestias maxime corporis quam repellat sequi. Aliquam, 
+               beatae!</p>
+        </section>
+        <section>
+            <h2>Imagem interna</h2>
+            <figure>
+                <img
+          src="./quadrado-b.png"
+          alt="Imagem ilustrativa do conteúdo"
+            />
+            </figure>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus 
+               sapiente tempora ex dicta ullam, sunt ea sit explicabo molestias similique 
+               corrupti unde ipsam magnam, illo atque voluptates deleniti placeat quod
+               dolor veniam. Dolorum modi, aperiam numquam architecto, unde reprehenderit 
+               similique iusto iure molestias maxime corporis quam repellat sequi. Aliquam, 
+               beatae!</p>
+        </section>
+    </main>
+  )
+}
