defb11bbae719f04421d1b2244ac7d904902dcbd
matheusfa08 <littlepacoquinha@gmail.com>
2026-09-03 11:53:07 -0300

    feat: Aplicando react-routes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,12 @@
+import { Outlet } from 'react-router'
 import Cabecalho from './components/Cabecalho/index.tsx'
-import Conteudo from './components/Conteudo/index.tsx'
 import Rodape from './components/Rodape/index.tsx'
 
 export default function App() {
   return (
     <>
       <Cabecalho />
-      <Conteudo />
+      <Outlet />
       <Rodape />
     </>
   )
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,9 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
 import App from './App.tsx'
+import EditarProduto from './routes/EditarProduto/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
+import Home from './routes/Home/index.tsx'
+import Error from './routes/Error/index.tsx'
+
+const router = createBrowserRouter([
+  { 
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/produtos',
+        element: <Produtos />
+      },
+      {
+        path: '/produtos/:id',
+        element: <EditarProduto />
+      }
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
