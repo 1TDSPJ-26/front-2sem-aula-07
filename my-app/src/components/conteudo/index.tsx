5b57996d71a17843c614016911f6aef24dce1a15
juansouzamarques <juan.smarques2007@gmail.com>
2026-09-03 11:53:34 -0300

    feat: manipulacao de imagens e importacoes dentro do react

--- my-app/src/components/conteudo/index.tsx
+++ my-app/src/components/conteudo/index.tsx
@@ -1,8 +1,41 @@
+import imgQuadrado from "../../img/quadrado (2).png"
+
 export default function App() {
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur veritatis delectus exercitationem dolorum iste soluta provident eveniet officiis, est illo impedit, culpa, itaque porro voluptatum at odio praesentium nam architecto beatae? Libero accusantium inventore aperiam dolorem nulla aliquid sit eligendi fuga facere totam, ad suscipit officiis neque ut eius? A?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nemo commodi rerum molestias voluptas explicabo doloribus nihil officia autem repellendus! Distinctio officiis suscipit aperiam minus dignissimos pariatur nihil quam corporis dicta omnis? Ducimus corporis minima quod illum ut, officiis nobis! Dolorum obcaecati eligendi esse ullam soluta quis natus, quisquam inventore?</p>
-    </div>
+    <main>
+        <section>
+            <h2>Conteudo</h2>
+            <figure>
+                    <img src="/quadrado-b.png" alt="Imagem ilustrativa do conteúdo" />
+                <figcaption>
+                 Imagem utilizada para representar o conteúdo da aplicação.
+                </figcaption>
+            </figure>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur veritatis delectus exercitationem dolorum iste soluta provident eveniet officiis, est illo impedit, culpa, itaque porro voluptatum at odio praesentium nam architecto beatae? Libero accusantium inventore aperiam dolorem nulla aliquid sit eligendi fuga facere totam, ad suscipit officiis neque ut eius? A?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nemo commodi rerum molestias voluptas explicabo doloribus nihil officia autem repellendus! Distinctio officiis suscipit aperiam minus dignissimos pariatur nihil quam corporis dicta omnis? Ducimus corporis minima quod illum ut, officiis nobis! Dolorum obcaecati eligendi esse ullam soluta quis natus, quisquam inventore?</p>
+      </section>
+        <section>
+            <h2>Conteudo</h2>
+            <figure>
+                    <img src="https://placehold.co/300x200/FF0000/FFFFF0/png" alt="Imagem ilustrativa do conteúdo" />
+                <figcaption>
+                 Imagem utilizada para representar o conteúdo da aplicação.
+                </figcaption>
+            </figure>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur veritatis delectus exercitationem dolorum iste soluta provident eveniet officiis, est illo impedit, culpa, itaque porro voluptatum at odio praesentium nam architecto beatae? Libero accusantium inventore aperiam dolorem nulla aliquid sit eligendi fuga facere totam, ad suscipit officiis neque ut eius? A?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nemo commodi rerum molestias voluptas explicabo doloribus nihil officia autem repellendus! Distinctio officiis suscipit aperiam minus dignissimos pariatur nihil quam corporis dicta omnis? Ducimus corporis minima quod illum ut, officiis nobis! Dolorum obcaecati eligendi esse ullam soluta quis natus, quisquam inventore?</p>
+      </section>
+              <section>
+            <h2>Conteudo</h2>
+            <figure>
+                    <img src={imgQuadrado} alt="Imagem ilustrativa do conteúdo" />
+                <figcaption>
+                 Imagem utilizada para representar o conteúdo da aplicação.
+                </figcaption>
+            </figure>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur veritatis delectus exercitationem dolorum iste soluta provident eveniet officiis, est illo impedit, culpa, itaque porro voluptatum at odio praesentium nam architecto beatae? Libero accusantium inventore aperiam dolorem nulla aliquid sit eligendi fuga facere totam, ad suscipit officiis neque ut eius? A?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nemo commodi rerum molestias voluptas explicabo doloribus nihil officia autem repellendus! Distinctio officiis suscipit aperiam minus dignissimos pariatur nihil quam corporis dicta omnis? Ducimus corporis minima quod illum ut, officiis nobis! Dolorum obcaecati eligendi esse ullam soluta quis natus, quisquam inventore?</p>
+      </section>
+    </main>
   )
 }
