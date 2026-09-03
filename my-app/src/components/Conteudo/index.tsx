4e244698c23ba4af31a8d67aab6e47ee72ddb3bc
Ch-Rodrigues <christianprodrigues07@gmail.com>
2026-09-03 11:34:57 -0300

    feat : Realizando a modularização

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,10 @@
+export function Conteudo() {
+    return (
+        <main>
+            <section>
+            <h2>Bem-vindo ao meu site!</h2>
+            <p>Este é o conteúdo principal da página.</p>
+            </section>
+        </main>
+    );
+}
