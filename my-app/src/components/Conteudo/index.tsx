7456c535be21b109dfa9390f8dfa4a4c87aadde8
Renato Ruiz <ruizrenato511@gmail.com>
2026-09-03 11:49:47 -0300

    feat :trabalhando com imagens

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,7 +1,26 @@
+import imgQuadrado from "../../img/quadrado.png";
+
+
 export default function Conteudo() {
   return (
     <main>
-      <p>Conteúdo da página.</p>
+      <section>
+        <h2>Conteúdo</h2>
+        <figure>
+          <img src="https://placehold.co/300x200/FF0000/FFFFFF/png" alt="Imagem ilustrativa do conteúdo" />
+          <figcaption>Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
+        </figure>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
+      </section>
+
+      <section>
+        <h2>Imagem Interna</h2>
+        <figure>
+          <img src="https://placehold.co/300x200/0000FF/FFFFFF/png" alt="Imagem ilustrativa do conteúdo" />
+          <figcaption>Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
+        </figure>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
+      </section>
     </main>
   );
 }
