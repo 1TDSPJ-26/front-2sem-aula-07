ab5e88d3c82aa4e1cac804a8ba6002b6c2203f6e
JoaoVitor-2209 <vitorcruzrocha@gmail.com>
2026-09-03 11:50:46 -0300

    feat : Estruturando os hyperlinks de imagem.

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,18 +1,52 @@
+import imgQuadrado from "../../img/quadrado.png";
 export default function Conteudo() {
   return (
     <main>
-      <h2>Conteúdo principal</h2>
+      <section>
+        <h2>Conteúdo principal</h2>
 
       <figure>
         <img
-          src="https://placehold.co/600x400/000000/FFFFFF/png"
+          src="https://placehold.co/300x200/000000/FFFFFF/png"
           alt="Imagem ilustrativa do conteúdo"
         />
 
         <figcaption>
           Imagem utilizada para representar o conteúdo da aplicação.
         </figcaption>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci accusantium quos debitis. Quo amet dolorem aperiam enim asperiores quam magni exercitationem ratione, suscipit officia porro tenetur esse id, perferendis recusandae tempore ullam accusantium ex assumenda officiis blanditiis eius quibusdam. Impedit quibusdam labore rem accusamus, ducimus consectetur perspiciatis ipsum dolor tempore.</p>
       </figure>
+      </section>
+      <section>
+        <h2>Imagem Interna</h2>
+
+      <figure>
+        <img
+          src={imgQuadrado}
+          alt="Imagem ilustrativa do conteúdo"
+        />
+
+        <figcaption>
+          Imagem utilizada para representar o conteúdo da aplicação.
+        </figcaption>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci accusantium quos debitis. Quo amet dolorem aperiam enim asperiores quam magni exercitationem ratione, suscipit officia porro tenetur esse id, perferendis recusandae tempore ullam accusantium ex assumenda officiis blanditiis eius quibusdam. Impedit quibusdam labore rem accusamus, ducimus consectetur perspiciatis ipsum dolor tempore.</p>
+      </figure>
+      </section>
+      <section>
+        <h2>Imagem Estatica</h2>
+
+      <figure>
+        <img
+          src="/quadrado-b.png"
+          alt="Imagem ilustrativa do conteúdo"
+        />
+
+        <figcaption>
+          Imagem utilizada para representar o conteúdo da aplicação.
+        </figcaption>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci accusantium quos debitis. Quo amet dolorem aperiam enim asperiores quam magni exercitationem ratione, suscipit officia porro tenetur esse id, perferendis recusandae tempore ullam accusantium ex assumenda officiis blanditiis eius quibusdam. Impedit quibusdam labore rem accusamus, ducimus consectetur perspiciatis ipsum dolor tempore.</p>
+      </figure>
+      </section>
     </main>
   )
 }
