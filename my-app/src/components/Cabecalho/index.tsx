17ec2693f6cae23616481c5af7d45610acb99e95
leite-1309 <leite.lucas1309@gmail.com>
2026-09-03 10:54:09 -0300

    feat: adiciona componentes e favicon

---
+++ my-app/src/components/Cabecalho/index.tsx
@@ -0,0 +1,7 @@
+export default function Cabecalho() {
+  return (
+    <main>
+      <h1>Cabeçalho</h1>
+    </main>
+  )
+}
---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,15 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <h2>Conteúdo Principal</h2>
+
+      <figure>
+        <img src="https://placehold.co/600x400/FFFFF0/FFFFFF/png" alt="Imagem ilustrativa do conteúdo" />
+
+        <figcaption>
+            Imagem utilizada para representar o contéudo da aplicação.
+        </figcaption>
+      </figure>
+    </main>
+  )
+}
