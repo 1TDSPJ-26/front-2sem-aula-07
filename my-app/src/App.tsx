7e5cc38f26e6df1c033ee3dfaadaa646366c7dd4
Ch-Rodrigues <christianprodrigues07@Gmail.com>
2026-09-04 11:56:01 -0300

    feat : Exercicio da lampada

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,15 @@
-import Cabecalho from './components/Cabecalho';
-import { Conteudo } from './components/Conteudo';
-import Rodape from './components/Rodape';
+import { Outlet } from 'react-router'
+import Cabecalho from './components/Cabecalho'
+import Rodape from './components/Rodape'
+import Lampada from './components/Lampada'
 
 export default function App() {
   return (
-    <div>
-      <Cabecalho/>
-      <Conteudo/>
-      <Rodape/>
-    </div>
-  );
+    <>
+      <Cabecalho />
+      <Outlet />
+      <Rodape />
+      <Lampada />
+    </>
+  )
 }
